f57ace0e833b4ff7c762b34c3bfa0c5c79601c91
daramacmeanmain <dara.macmeanmain@outlook.ie>
2016-11-29 13:14:41 +0000

    Database basic user input and output

--- db.py
+++ db.py
@@ -6,15 +6,10 @@ conn = sqlite3.connect(DATABASE)
 c = conn.cursor()
 
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS userTable(id INTEGER, name TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS messageTable(message TEXT)')
     
-def data_entry():
-    id = 123
-    name = 'Test'
-    c.execute("INSERT INTO userTable (id, name) VALUES(?, ?)", (id, name))
-    conn.commit()
-    
+
 
 if __name__ == "__main__":
     create_table()
-    data_entry()
+    ##data_entry()
--- webapp.py
+++ webapp.py
@@ -1,7 +1,10 @@
-import flask as fl
+import flask as fl 
+from flask import render_template, request, g
 import sqlite3
 
 DATABASE = 'data/project.db'
+conn = sqlite3.connect(DATABASE)
+c = conn.cursor()
 
 app = fl.Flask(__name__)
 
@@ -15,18 +18,26 @@ def get_db():
         db = fl.g._database = sqlite3.connect(DATABASE)
     return db
 
+@app.route('/data', methods = ['GET', 'POST'])        
+def update_db():
+    c.execute("INSERT INTO messageTable(message) VALUES(?)",(fl.request.form['messageInput'],))
+    conn.commit()
+    return str(c.fetchall())
+
+@app.route('/dataBase', methods=["GET", "POST"])
+def user_data():
+    conn = sqlite3.connect(DATABASE)
+    c = get_db().cursor()
+    c.execute("SELECT * FROM messageTable")
+    return str(c.fetchall())
+
+
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(fl.g, '_database', None)
     if db is not None:
         db.close()
-
-@app.route('/data', methods=["GET", "POST"])
-def user_data():
-    conn = sqlite3.connect(DATABASE)
-    c = get_db().cursor()
-    c.execute("SELECT * FROM userTable")
-    return str(c.fetchall())
 
 if __name__ == "__main__":
     app.run()
