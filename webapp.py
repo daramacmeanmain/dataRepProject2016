34d370ab035c3bcb00f7bc47872973b0827464d0
daramacmeanmain <dara.macmeanmain@outlook.ie>
2016-11-30 23:13:46 +0000

    db output with render_template - kinda broken atm

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 import flask as fl 
-from flask import request, g
+from flask import render_template, request, g
 import sqlite3
 
 DATABASE = 'data/project.db'
@@ -10,7 +10,7 @@ app = fl.Flask(__name__)
 
 @app.route("/")
 def root():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 def get_db():
     db = getattr(fl.g, '_database', None)
@@ -30,15 +30,31 @@ def update_messageData():
     conn.commit()
     return str(c.fetchall())
 
+##@app.route('/userDataBase', methods=["GET", "POST"])
+##def user_data():
+##    conn = sqlite3.connect(DATABASE)
+##    c = get_db().cursor()
+    ##for row in c.execute("SELECT message FROM messageTable ORDER BY id DESC"):
+    ##message = row[0]
+##    c.execute("SELECT user FROM userTable ORDER BY userId DESC")
+##    return str(c.fetchall())
+
+##@app.route('/messageDataBase', methods=["GET", "POST"])
+##def message_data():
+##    conn = sqlite3.connect(DATABASE)
+##    c = get_db().cursor()
+ ##   c.execute("SELECT message FROM messageTable ORDER BY messageId DESC")
+##    return str(c.fetchall())
+
 @app.route('/dataBase', methods=["GET", "POST"])
 def user_data():
     conn = sqlite3.connect(DATABASE)
     c = get_db().cursor()
     ##for row in c.execute("SELECT message FROM messageTable ORDER BY id DESC"):
     ##message = row[0]
-    c.execute("SELECT user, message FROM messageTable INNER JOIN userTable on userTable.userId = messageTable.messageId ORDER BY messageId DESC")
-    return str(c.fetchall())
-
+    dataOutput = c.execute("SELECT user, message FROM messageTable INNER JOIN userTable on userTable.userId = messageTable.messageId ORDER BY messageId DESC").fetchall()
+    return render_template('index.html', dataOutput=dataOutput)
+    
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(fl.g, '_database', None)
