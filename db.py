9605f010dbf747e567b6d8bdb7fa54c3f1231ad0
daramacmeanmain <dara.macmeanmain@outlook.ie>
2016-12-01 19:25:34 +0000

    table outputs unique entries. tidied and commented code

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
+#Adapted from https://github.com/data-representation/example-sqlite
+
 import sqlite3
 
 DATABASE = 'data/project.db'
@@ -5,10 +7,10 @@ DATABASE = 'data/project.db'
 conn = sqlite3.connect(DATABASE)
 c = conn.cursor()
 
+#Create tables
 def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS messageTable(messageId INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)')
     c.execute('CREATE TABLE IF NOT EXISTS userTable(userId INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT)')
 
 if __name__ == "__main__":
     create_table()
-    ##data_entry()
--- webapp.py
+++ webapp.py
@@ -1,3 +1,5 @@
+#Adapted from https://github.com/data-representation/example-sqlite
+
 import flask as fl 
 from flask import render_template, request, g
 import sqlite3
@@ -18,48 +20,35 @@ def get_db():
         db = fl.g._database = sqlite3.connect(DATABASE)
     return db
 
+#Insert data from Name Input
 @app.route('/userData', methods = ['GET', 'POST'])        
 def update_userData():
     c.execute("INSERT INTO userTable(user) VALUES(?)",(fl.request.form['userInput'],))
     conn.commit()
     return str(c.fetchall())
 
-@app.route('/messageData', methods = ['GET', 'POST'])        
+#Insert data from Message Input
+@app.route('/messageData', methods = ['GET', 'POST'])
 def update_messageData():
     c.execute("INSERT INTO messageTable(message) VALUES(?)",(fl.request.form['messageInput'],))
     conn.commit()
     return str(c.fetchall())
 
-##@app.route('/userDataBase', methods=["GET", "POST"])
-##def user_data():
-##    conn = sqlite3.connect(DATABASE)
-##    c = get_db().cursor()
-    ##for row in c.execute("SELECT message FROM messageTable ORDER BY id DESC"):
-    ##message = row[0]
-##    c.execute("SELECT user FROM userTable ORDER BY userId DESC")
-##    return str(c.fetchall())
-
-##@app.route('/messageDataBase', methods=["GET", "POST"])
-##def message_data():
-##    conn = sqlite3.connect(DATABASE)
-##    c = get_db().cursor()
- ##   c.execute("SELECT message FROM messageTable ORDER BY messageId DESC")
-##    return str(c.fetchall())
-
+#Output data from the database to index.html
 @app.route('/dataBase', methods=["GET", "POST"])
 def user_data():
     conn = sqlite3.connect(DATABASE)
     c = get_db().cursor()
-    ##for row in c.execute("SELECT message FROM messageTable ORDER BY id DESC"):
-    ##message = row[0]
-    dataOutput = c.execute("SELECT user, message FROM messageTable INNER JOIN userTable on userTable.userId = messageTable.messageId ORDER BY messageId DESC LIMIT 1").fetchall()
+    dataOutput = c.execute("SELECT DISTINCT user, message FROM messageTable INNER JOIN userTable on userTable.userId = messageTable.messageId ORDER BY messageId DESC").fetchall()
     return render_template('index.html', dataOutput=dataOutput)
-    
+
+#Close database
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(fl.g, '_database', None)
     if db is not None:
         db.close()
 
+#run app
 if __name__ == "__main__":
     app.run()
